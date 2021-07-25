07660526665e2a856d75d7f486aa327df4997050
moekify <19408385+moekify@users.noreply.github.com>
2021-07-25 18:15:21 +0200

    Run prettier on index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Listr from "listr";
 
 import commandLineArgs from "command-line-args";
-import * as fs from 'fs';
+import * as fs from "fs";
 import { migrateSchema } from "./tasks/schema.js";
 import { migrateFiles } from "./tasks/files.js";
 import { migrateUsers } from "./tasks/users.js";
@@ -9,20 +9,20 @@ import { migrateData } from "./tasks/data.js";
 import { migrateRelations } from "./tasks/relations.js";
 
 const commandLineOptions = commandLineArgs([
-	{
-		name: "skipCollections",
-		alias: "s",
-		type: String,
-		multiple: true,
-		defaultValue: [],
-	},
-	{
-		name: "useContext",
-		alias: "c",
-		type: String,
-		multiple: false,
-		defaultValue: './context/start.json'
-	},
+  {
+    name: "skipCollections",
+    alias: "s",
+    type: String,
+    multiple: true,
+    defaultValue: [],
+  },
+  {
+    name: "useContext",
+    alias: "c",
+    type: String,
+    multiple: false,
+    defaultValue: "./context/start.json",
+  },
 ]);
 
 const tasks = new Listr([
@@ -32,7 +32,9 @@ const tasks = new Listr([
   },
   {
     title: "Migrating Schema",
-		skip: context => context.completedSteps.schema === true && context.completedSteps.collections === true,
+    skip: (context) =>
+      context.completedSteps.schema === true &&
+      context.completedSteps.collections === true,
     task: (context) => {
       context.skipCollections = commandLineOptions.skipCollections;
       return migrateSchema(context);
@@ -40,22 +42,24 @@ const tasks = new Listr([
   },
   {
     title: "Migration Files",
-		skip: context => context.completedSteps.files === true,
+    skip: (context) => context.completedSteps.files === true,
     task: migrateFiles,
   },
   {
     title: "Migrating Users",
-		skip: context => context.completedSteps.roles === true && context.completedSteps.users === true,
+    skip: (context) =>
+      context.completedSteps.roles === true &&
+      context.completedSteps.users === true,
     task: migrateUsers,
   },
   {
     title: "Migrating Relations",
-    skip: context => context.completedSteps.relations === true,
+    skip: (context) => context.completedSteps.relations === true,
     task: migrateRelations,
   },
   {
     title: "Migrating Data",
-		skip: context => context.completedSteps.data === true,
+    skip: (context) => context.completedSteps.data === true,
     task: migrateData,
   },
 
@@ -67,33 +71,34 @@ const tasks = new Listr([
 
 export async function writeContext(context, section) {
   context.completedSteps[section] = true;
-	await fs.promises.writeFile(`./context/${section}.json`, JSON.stringify(context));
+  await fs.promises.writeFile(
+    `./context/${section}.json`,
+    JSON.stringify(context)
+  );
 }
 
 async function setupContext(context) {
-	const contextJSON = await fs.promises.readFile(
+  const contextJSON = await fs.promises.readFile(
     commandLineOptions.useContext,
-    'utf8'
+    "utf8"
   );
-  console.log('Loading context')
+  console.log("Loading context");
   const fetchedContext = JSON.parse(contextJSON);
   Object.entries(fetchedContext).forEach(([key, value]) => {
     context[key] = value;
   });
-	console.log(
-		`✨ Loaded context succesfully`
-	)
+  console.log(`✨ Loaded context succesfully`);
 }
 
 console.log(
-	`✨ Migrating ${process.env.V8_URL} (v8) to ${process.env.V9_URL} (v9)...`
+  `✨ Migrating ${process.env.V8_URL} (v8) to ${process.env.V9_URL} (v9)...`
 );
 
 tasks
-	.run()
-	.then(() => {
-		console.log("✨ All set! Migration successful.");
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+  .run()
+  .then(() => {
+    console.log("✨ All set! Migration successful.");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
