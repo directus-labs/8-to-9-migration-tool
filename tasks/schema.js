ed4751f8ffa3964dcc3f4f759a6ba42e7997c75e
Jose Varela <joselcvarela@gmail.com>
2022-07-25 14:54:16 +0100

    Handle UUID if relation points to `directus_files`

--- tasks/schema.js
+++ tasks/schema.js
@@ -203,6 +203,17 @@ function migrateCollection(collection, context) {
 			},
 			schema: {},
 			fields: Object.values(collection.fields).map((details) => {
+        const relation =
+          context.relationsV8Map?.[collection.collection]?.[details.field] ||
+          {};
+
+        const isUuid =
+          details.field.includes("directus_files_id") ||
+          details.interface === "user-roles" ||
+          ["directus_files", "directus_users"].includes(
+            relation?.collection_one
+          );
+
 				return {
 					field: details.field,
 					type:
@@ -211,8 +222,7 @@ function migrateCollection(collection, context) {
 							? "text"
 							: details.interface === "many-to-many"
 							? "m2m"
-							: details.field.includes("directus_files_id") ||
-							  details.interface === "user-roles"
+              : isUuid
 							? "uuid"
 							: typeMap[details.type.toLowerCase()],
 					meta: {
